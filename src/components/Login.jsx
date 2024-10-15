a4c47054d417b5f0753a217d13c679780ec2743e
Naveen <naveenapk048@gmail.com>
2024-10-15 20:49:26 +0530

    updated password listing component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -21,16 +21,19 @@ const Login = () => {
     }
   };
   return (
-    <div className='h-svh flex justify-center items-center w-full bg-cover' style={{ "background": "url('https://res.cloudinary.com/dwxhfdats/image/upload/v1722612846/cdefpcbxnrmcts1twuu4.jpg')" }}>
-      <div className='h-[500px] w-[500px] bg-orange-200 rounded-xl flex flex-col justify-between p-10'>
+    <div className='h-svh flex justify-center items-center w-full bg-cover bg-black'
+    //  style={{ "background": "url('https://res.cloudinary.com/dwxhfdats/image/upload/v1722612846/cdefpcbxnrmcts1twuu4.jpg')" }}
+     >
+      <div className='h-[380px] w-[500px] bg-black rounded-xl flex flex-col justify-between p-6 md:p-10'>
 
 
-        <p className='h-[350px] bg-red-400 p-3 break-words rounded-lg text-white font-semibold text-xl'>sdfjslffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffjlfj</p>
+        <p className='h-[330px] bg-gray-400 p-5 break-words rounded-lg text-white font-bold text-8xl'>Let's Lock it.</p>
+      
 
 
 
 
-        <button className='bg-red-500 p-3 px-16 rounded-lg text-white font-bold text-xl' onClick={GoogleLogin}>Continue With Google</button>
+        <button className='bg-cyan-500 p-3 mt-5 px-16 rounded-lg text-white font-bold text-xl' onClick={GoogleLogin}>Continue With Google</button>
       </div>
 
       {/* <button onClick={Logout}>Logout</button> */}
--- src/components/PasswordListing.jsx
+++ src/components/PasswordListing.jsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { auth, db } from '../firebase/index';
 import { collection, deleteDoc, doc, getDocs, query, where } from 'firebase/firestore';
 import { onAuthStateChanged } from 'firebase/auth';
+import {toast} from 'react-toastify'
 
 const PasswordListing = () => {
     const [passwords, setPasswords] = useState([]);
     const [user, setUser] = useState(null);
+    const [refresh,setRefresh]=useState(false)
 
     useEffect(() => {
         // Listen to auth state changes to ensure the user is available
@@ -21,7 +23,7 @@ const PasswordListing = () => {
 
         // Cleanup subscription on unmount
         return () => unsubscribe();
-    }, []);
+    }, [refresh]);
 
     const fetchPasswords = async (currentUser) => {
         if (currentUser) {
@@ -56,6 +58,8 @@ const PasswordListing = () => {
         try {
           const docRef = doc(db, 'Passwords', documentId);
           await deleteDoc(docRef);
+          toast('Password deleted successfully')
+          setRefresh(prev=>!prev)
           console.log('Document deleted successfully');
         } catch (error) {
           console.error('Error deleting document:', error);   
@@ -63,109 +67,39 @@ const PasswordListing = () => {
         }
       }
 
+      const copyToClipboard = (text) => {
+        navigator.clipboard.writeText(text)
+            .then(() => {
+                console.log('Text copied to clipboard', text);
+                toast('Copied to clipboard', {
+                    toastId: 'copyToClipboard',
+                });
+            })
+            .catch(err => {
+                console.error('Failed to copy: ', err);
+            });
+    }
+
     return (
         <div className='w-full h-5/6 flex justify-center items-center '>
-        <div className='max-h-[600px] overflow-auto py-30 bg-red-400 mt-16 w-[500px] '>
+        <div className='max-h-[600px] overflow-auto py-30  mt-16 w-[500px] '>
             <div className='p-3 '>
-                <h2>PasswordListing</h2>
+                {passwords.length ? <h2 className='text-2xl text-center font-bold'>Saved Passwords</h2> : <h2 className='text-2xl text-center font-bold'>No Saved Passwords</h2>}
             </div>
             {/* Render the passwords */}
             <ul>
-                {passwords.map((password) => (
+                {passwords.length ? passwords.map((password) => (
                     <>
-                     <li key={password.id}>{password.password} <button onClick={()=>deleteDocument(password.id)}>delete</button></li>                   
-                    </>
-                ))}
-                {passwords.map((password) => (
-                    <>
-                     <li key={password.id}>{password.password} <button onClick={()=>deleteDocument(password.id)}>delete</button></li>                   
-                    </>
-                ))}
-                {passwords.map((password) => (
-                    <>
-                     <li key={password.id}>{password.password} <button onClick={()=>deleteDocument(password.id)}>delete</button></li>                   
-                    </>
-                ))}
-                {passwords.map((password) => (
-                    <>
-                     <li key={password.id}>{password.password} <button onClick={()=>deleteDocument(password.id)}>delete</button></li>                   
-                    </>
-                ))}
-                {passwords.map((password) => (
-                    <>
-                     <li key={password.id}>{password.password} <button onClick={()=>deleteDocument(password.id)}>delete</button></li>                   
-                    </>
-                ))}
-                {passwords.map((password) => (
-                    <>
-                     <li key={password.id}>{password.password} <button onClick={()=>deleteDocument(password.id)}>delete</button></li>                   
-                    </>
-                ))}
-                {passwords.map((password) => (
-                    <>
-                     <li key={password.id}>{password.password} <button onClick={()=>deleteDocument(password.id)}>delete</button></li>                   
-                    </>
-                ))}
-                {passwords.map((password) => (
-                    <>
-                     <li key={password.id}>{password.password} <button onClick={()=>deleteDocument(password.id)}>delete</button></li>                   
-                    </>
-                ))}
-                {passwords.map((password) => (
-                    <>
-                     <li key={password.id}>{password.password} <button onClick={()=>deleteDocument(password.id)}>delete</button></li>                   
-                    </>
-                ))}
-                {passwords.map((password) => (
-                    <>
-                     <li key={password.id}>{password.password} <button onClick={()=>deleteDocument(password.id)}>delete</button></li>                   
-                    </>
-                ))}
-                {passwords.map((password) => (
-                    <>
-                     <li key={password.id}>{password.password} <button onClick={()=>deleteDocument(password.id)}>delete</button></li>                   
-                    </>
-                ))}
-                {passwords.map((password) => (
-                    <>
-                     <li key={password.id}>{password.password} <button onClick={()=>deleteDocument(password.id)}>delete</button></li>                   
-                    </>
-                ))}
-                {passwords.map((password) => (
-                    <>
-                     <li key={password.id}>{password.password} <button onClick={()=>deleteDocument(password.id)}>delete</button></li>                   
-                    </>
-                ))}
-                {passwords.map((password) => (
-                    <>
-                     <li key={password.id}>{password.password} <button onClick={()=>deleteDocument(password.id)}>delete</button></li>                   
-                    </>
-                ))}
-                {passwords.map((password) => (
-                    <>
-                     <li key={password.id}>{password.password} <button onClick={()=>deleteDocument(password.id)}>delete</button></li>                   
-                    </>
-                ))}
-                {passwords.map((password) => (
-                    <>
-                     <li key={password.id}>{password.password} <button onClick={()=>deleteDocument(password.id)}>delete</button></li>                   
-                    </>
-                ))}
-                {passwords.map((password) => (
-                    <>
-                     <li key={password.id}>{password.password} <button onClick={()=>deleteDocument(password.id)}>delete</button></li>                   
-                    </>
-                ))}
-                {passwords.map((password) => (
-                    <>
-                     <li key={password.id}>{password.password} <button onClick={()=>deleteDocument(password.id)}>delete</button></li>                   
-                    </>
-                ))}
-                {passwords.map((password) => (
-                    <>
-                     <li key={password.id}>{password.password} <button onClick={()=>deleteDocument(password.id)}>delete</button></li>                   
-                    </>
-                ))}
+                    <div key={password.id} className='flex bg-black m-1 p-2 justify-around rounded-lg'>
+                        <div className='p-2 bg-slate-400 flex justify-center items-center m-2 w-1/2 rounded-lg text-white font-semibold text-xl'>{password.password}</div>
+                        <div className='flex flex-col '>
+                            <button className='flex justify-center items-center p-1 m-1 bg-red-500 text-white font-medium w-full rounded-md' onClick={()=>deleteDocument(password.id)}>delete</button>
+                            <button className='flex justify-center items-center p-1 m-1 bg-white w-full rounded-md font-medium' onClick={()=>copyToClipboard(password.password)}>copy to clipboard</button>
+                        </div>
+                    </div>
+                    </> 
+                )) : ''}
+                
             </ul>
         </div>
         </div>
